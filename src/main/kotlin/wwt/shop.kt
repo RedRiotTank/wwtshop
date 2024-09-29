837d514aa39c1f2002b0a737f79ed1623fff5475
AlbertoPlaza <albertoplazamontesdm@gmail.com>
2024-09-29 21:11:32 +0200

    FIRST COMMIT

---
+++ src/main/kotlin/wwt/shop.kt
@@ -0,0 +1,14 @@
+package wwt
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class shop : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
